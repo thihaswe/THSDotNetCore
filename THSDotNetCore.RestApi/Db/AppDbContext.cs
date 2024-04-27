b6cd01f5326fedff0bc827799803b12fc89d7c38
thihaswe <thihais123@gmail.com>
2024-04-27 11:14:59 +0630

    efcore and restapi

---
+++ THSDotNetCore.RestApi/Db/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THSDotNetCore.RestApi.Models;
+
+namespace THSDotNetCore.RestApi.Db
+{
+    internal class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+
+
+    }
+}
---
+++ THSDotNetCore.RestApi/Models/BlogModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THSDotNetCore.RestApi.Models;
+
+[Table("Tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+
+    public string BlogTitle { get; set; }
+
+    public string BlogAuthor { get; set; }
+
+    public string BlogContent { get; set; }
+}
+
+//public record BLogEntity(int BlogId,string BlogTitle,string BlogAuthor,string BlogContent);
